50023f4301475faef0499b1735474f42a9b47cb4
michaelthehack9 <77589350+michaelthehack9@users.noreply.github.com>
2025-07-24 18:15:29 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,12 +10,10 @@ read api_key
 # Save the key to a file
 echo "$api_key" > key.txt
 
-# Move the main scripts out
-mv HAHSICS/audit.sh audit.sh
+# Move the main script out
 mv HAHSICS/snipeit.sh snipeit.sh
 
 # Give run permissions
-chmod +x audit.sh
 chmod +x snipeit.sh
 
 # Remove junk
